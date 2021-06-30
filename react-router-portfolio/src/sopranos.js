fd33d8bd9de00e652aea99f5b1c05e9eb596c83f
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-06-30 11:58:02 -0400

    data and images

--- react-router-portfolio/src/sopranos.js
+++ react-router-portfolio/src/sopranos.js
@@ -6,67 +6,78 @@ const sopranos = [
             name: 'Anthony "Tony" Soprano',
             description: 'CAPO',
             lives: false,
+            img: './img/tony.jpeg',
             id: 1
         },
         {
             name: 'Christopher Moltisanti',
             description: 'Soldier',
             lives: false,
+            img: './img/chris.jpeg',
             id: 2
         },
         {
             name: 'Furio',
             description: 'Soldier',
             lives: true,
+            img: './img/furio.jpg',
             id: 3
         },
         {
             name: 'Hesh Rabkin',
             description: 'Advisor',
             lives: true,
+            img: './img/hesh.jpeg',
             id: 4
         },
         {
             name: 'Paulie Gualtieri',
             description: 'CAPO',
             lives: true,
+            img: './img/paulie.jpeg',
             id: 5
         },
         {
             name: 'Salvatore "Big Pussy" Bonpensiero',
             description: 'CAPO',
             lives: false,
+            img: './img/Big_Pussy.jpeg',
             id: 6
         },
         {
             name: 'Silvio Dante',
             description: 'Consigliere',
             lives: false,
+            img: './img/silvio.jpeg',
             id: 7
         },
         {
             name: 'Tony Blundetto',
             description: 'Soldier',
             lives: false,
+            img: './img/tonyb.jpg',
             id: 8
         },
         {
             name: 'Vito Spatafore',
             description: 'Soldier',
             lives: false,
+            img: './img/vito.png',
             id: 9
         },
         {
             name: 'Richie Apriele',
             description: 'CAPO',
             lives: false,
+            img: './img/Richie.jpeg',
             id: 10
         },
         {
             name: 'Corrado "junior" Soprano',
             description: 'BOSS',
             lives: true,
-            id: 10
+            img: './img/junior.jpeg',
+            id: 11
         }
         ],
         id: 1
